cb11ca81991021d5c1f163f374dd8d419c2b4e83
VitorDiToro <vitorrditoro@gmail.com>
2020-08-26 06:35:26 -0300

    Update repository endpoint
    Basic implementation of the update repository endpoint

--- src/app.js
+++ src/app.js
@@ -30,7 +30,27 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if(repositoryIndex < 0)
+  {
+    return response.status(400).json({error: "Repository not found."});
+  }
+
+  const repository = {
+    id: repositories[repositoryIndex].id,
+    title: title,
+    url: url,
+    techs: techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
